9c44cb3af42c6257689fe7747419a0270af15ab8
Alex <alex.hoch@gmx.de>
2021-01-29 11:46:24 +0100

    Initial Commit

---
+++ MLImageClassifier_doz_17/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MLImageClassifier_doz_17
+//
+//  Created by Alexander Hoch on 29.01.21.
+//  Copyright © 2021 zancor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
